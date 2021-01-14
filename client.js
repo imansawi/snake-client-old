2e279a403c0a68068b9fb6c9faf2db3672d73ae2
imansawi <imansawi@hotmail.com>
2021-01-14 06:47:33 +0000

    file is created and connect function is added to it and  exported

---
+++ client.js
@@ -0,0 +1,17 @@
+// Establishes connection with the game server
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+}
+
+module.exports = connect;
